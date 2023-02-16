b14eeee093fd3d729003f5838f2ce2173a7e30f4
Rajvardhan <patilrajvardhan27@gmail.com>
2023-02-16 22:55:12 +0530

    using named routes

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -8,21 +8,22 @@ class CategoriesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Daily Meal'),
+        title: const Text('DeliMeal'),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(25),
         children: DUMMY_CATEGORIES
             .map(
               (catData) => CategoryItem(
-                catData.id,
-                catData.title,
-                catData.color,
-              ),
+                    catData.id,
+                    catData.title,
+                    catData.color,
+                  ),
             )
             .toList(),
         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 200,
-          childAspectRatio: 1.5,
+          childAspectRatio: 3 / 2,
           crossAxisSpacing: 20,
           mainAxisSpacing: 20,
         ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import './category_meals_screen.dart';
 import './categories_screen.dart';
 
 void main() => runApp(MyApp());
@@ -8,17 +9,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Daily Meal',
+      title: 'DeliMeals',
       theme: ThemeData(
         primarySwatch: Colors.pink,
         accentColor: Colors.amber,
         canvasColor: Color.fromRGBO(255, 254, 229, 1),
         fontFamily: 'Raleway',
         textTheme: ThemeData.light().textTheme.copyWith(
-            bodyMedium: TextStyle(
+            bodyLarge: TextStyle(
               color: Color.fromRGBO(20, 51, 51, 1),
             ),
-            bodyLarge: TextStyle(
+            bodyMedium: TextStyle(
               color: Color.fromRGBO(20, 51, 51, 1),
             ),
             titleMedium: TextStyle(
@@ -27,7 +28,12 @@ class MyApp extends StatelessWidget {
               fontWeight: FontWeight.bold,
             )),
       ),
-      home: CategoriesScreen(),
+      // home: CategoriesScreen(),
+      initialRoute: '/', // default is '/'
+      routes: {
+        '/': (ctx) => CategoriesScreen(),
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+      },
     );
   }
 }
