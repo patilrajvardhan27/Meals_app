656c2a838c3f8f1b2816bb14554bfc906cc7f789
Rajvardhan <patilrajvardhan27@gmail.com>
2023-02-19 11:48:26 +0530

    adding tab-bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import './screens/tabs_screen.dart';
 import './screens/meal_detail_screen.dart';
 import './screens/category_meals_screen.dart';
 import './screens/categories_screen.dart';
@@ -17,10 +18,10 @@ class MyApp extends StatelessWidget {
         canvasColor: Color.fromRGBO(255, 254, 229, 1),
         fontFamily: 'Raleway',
         textTheme: ThemeData.light().textTheme.copyWith(
-            bodyMedium: TextStyle(
+            bodyLarge: TextStyle(
               color: Color.fromRGBO(20, 51, 51, 1),
             ),
-            bodyLarge: TextStyle(
+            bodyMedium: TextStyle(
               color: Color.fromRGBO(20, 51, 51, 1),
             ),
             titleMedium: TextStyle(
@@ -32,14 +33,20 @@ class MyApp extends StatelessWidget {
       // home: CategoriesScreen(),
       initialRoute: '/', // default is '/'
       routes: {
-        '/': (ctx) => CategoriesScreen(),
+        '/': (ctx) => TabsScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
       },
       onGenerateRoute: (settings) {
         print(settings.arguments);
+        // if (settings.name == '/meal-detail') {
+        //   return ...;
+        // } else if (settings.name == '/something-else') {
+        //   return ...;
+        // }
+        // return MaterialPageRoute(builder: (ctx) => CategoriesScreen(),);
       },
-      onUnknownRoute: (settiings) {
+      onUnknownRoute: (settings) {
         return MaterialPageRoute(
           builder: (ctx) => CategoriesScreen(),
         );
